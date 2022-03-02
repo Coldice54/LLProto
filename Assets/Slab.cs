e9c7756b1c91099a6e4bc56f97a9311351b3ef22
shiirlee <shirley122007@gmail.com>
2022-03-01 16:31:07 -0800

    9slabPuzzle
    This is the full build of the 9 slab puzzle. when stepping on a block it and it's adjacent blocks the blocks will light and once all blocks light it will complete the puzzle

---
+++ Assets/Slab.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class Slab : MonoBehaviour
+{
+    public GameObject symbol;
+    public Material lightMat;
+    public Material darkMat;
+    public int index;
+    private Renderer rend;
+    public bool isLight = false;
+
+    private void Start() {
+        rend = symbol.GetComponent<Renderer>();
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.gameObject.tag == "Player") {
+            FindObjectOfType<NineSlab>().interact(index);
+        }
+    }
+
+    public void changeLight(){
+        if (isLight) {
+            rend.material = darkMat;
+            isLight = false;
+        } else {
+            rend.material = lightMat;
+            isLight = true;
+        }
+    }
+}
