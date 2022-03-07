b4c00d101b5964fb94ceed38ded1842345b67d61
Vance Winstead <vance.winstead@gmail.com>
2022-03-07 00:19:30 -0800

    Merge from master

---
+++ Assets/SymbolActive.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SymbolActive : MonoBehaviour
+{
+    public bool active = false;
+    [SerializeField] Material activeMaterial;
+    Renderer rend;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+    }
+
+    public void Activate()
+    {
+        active = true;
+        rend.material = activeMaterial;
+    }
+}
